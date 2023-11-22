dc60b1adc5ef60d95486778485a709e40eb318c7
aaron1792 <60758020+aaron1792@users.noreply.github.com>
2023-11-22 09:50:55 +0000

    commit 5

--- starter/script.js
+++ starter/script.js
@@ -420,4 +420,22 @@ Promise.allSettled([
   Promise.resolve('Success'),
   Promise.reject('ERROR'),
   Promise.resolve('Another success'),
-]);
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise.any [S2021]
+
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
